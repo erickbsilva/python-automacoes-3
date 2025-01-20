745b620db7ec412a623c620642487c0f09356b9d
Erick <erickbarretodasilva@gmail.com>
2025-01-20 18:04:08 -0300

    first commit

---
+++ 1-acessando-page.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# 1 - Utilizando o WebDriver
+browser = webdriver.Firefox()
+
+browser.get("http://www.amazon.com.br")
+
+# browser.quit()
+
+# 2 - Acessando elementos numa página
+elem = browser.find_element(By.ID, "twotabsearchtextbox")
+elem.send_keys("ps5")
+elem.send_keys(Keys.ENTER)
